2dadca2fb1c5a6606579f80548956b9b40bbaf98
adamg8539 <adamghule@outlook.com>
2025-04-30 23:48:00 +1000

    queue

---
+++ dsa-problems/stacks/implement-queue/README.md
@@ -0,0 +1,12 @@
+# Implement Queue using Stacks
+
+**Problem Links:**
+https://leetcode.com/problems/implement-queue-using-stacks/
+
+**Initial thoughts:** 
+
+**Efficient Method:** 
+
+| Leetcode Link | Time Complexity | Space Complexity | Solution beats % of LeetCoders |
+| --- | --- | --- | --- |
+| [Implement Queue](https://leetcode.com/problems/implement-queue-using-stacks/) | O(n) | O(n) | 100 |
---
+++ dsa-problems/stacks/implement-queue/implement-queue.py
@@ -0,0 +1,34 @@
+class MyQueue(object):
+
+    def __init__(self):
+        self.stack = []        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.stack.append(x)
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        val = self.stack[0]
+        self.stack = self.stack[1:]
+        return val
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+        return self.stack[0]
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        if self.stack:
+            return False
+        return True
+      
